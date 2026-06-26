5e95a15b450f62feefe66bdafb30b88a9a4b1c96
duy-tnb <duy-tnb@genkisystem.com>
2026-06-26 15:25:20 +0700

    chore: add ngrok domain to vite dev server allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["broderick-unflaked-unhastily.ngrok-free.dev"],
     port: 3000,
     open: true,
   },
